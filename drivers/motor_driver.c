bbffe4e9129e3f36ad12a88ee5f7d5caba86fc7c
GUNAPRASANTH P <113359609+Gunaprashanth480@users.noreply.github.com>
2026-03-10 16:31:07 +0530

    Create motor_driver.c

---
+++ drivers/motor_driver.c
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+#include "motor_driver.h"
+
+#define IN1 27
+#define IN2 26
+#define IN3 25
+#define IN4 33
+#define ENA 14
+#define ENB 12
+
+int speedValue = 200;
+
+void motorSpeed(int spd)
+{
+    speedValue = spd;
+    analogWrite(ENA, speedValue);
+    analogWrite(ENB, speedValue);
+}
+
+void stopMotors()
+{
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+}
+
+void forward()
+{
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+}
+
+void backward()
+{
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+}
+
+void left()
+{
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+}
+
+void right()
+{
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+}
